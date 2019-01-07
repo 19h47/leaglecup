7f39e6feee8cd16abece819232a34d5f5a9e76a8
19h47 <jeremylevron@19h47.fr>
2019-01-07 19:45:22 +0100

    :fire: Remove from master

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	// parser: 'sugarss',
-	plugins: {
-		// 'postcss-import': {},
-		// 'postcss-cssnext': {},
-		'autoprefixer': { browsers: ['last 2 versions', 'safari >= 8', 'ie >= 9', 'ff >= 20', 'ios 6', 'android 4'] },
-		'cssnano': process.env.NODE_ENV === 'production' ? true : false
-	}
-}
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import 'stylesheets/styles.scss';
-
-require.context('svg/', true);
-
-console.log('%c🔥 INES A (http://inesa.fr) & 19h47 (http://19h47.fr) 🔥', 'padding:0.5em 1em;' );
