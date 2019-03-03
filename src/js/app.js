55849910e644f0c2d37b53f2aca4f399dd15abaf
19h47 <jeremylevron@19h47.fr>
2019-03-04 00:28:11 +0100

    :zap: Scroll to

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 // import { name, version, repository } from '@/../package.json';
 import GuidGuidBangBang from 'Common/GuidGuidBangBang';
+import animateScrollTo from 'animated-scroll-to';
 
 // console.log(`%c${name}@${version} – ${repository.url}`, 'color: #6a6a6a');
 
@@ -8,5 +9,17 @@ import GuidGuidBangBang from 'Common/GuidGuidBangBang';
 		const guid = new GuidGuidBangBang();
 
 		guid.init();
+	}
+
+	// Scroll to button
+	const buttons = document.querySelectorAll('.js-scroll-to');
+
+	for (let i = 0; i < buttons.length; i += 1) {
+		const target = buttons[i].getAttribute('data-target');
+		const $el = document.getElementById(target);
+
+		buttons[i].addEventListener('click', () => {
+			animateScrollTo($el);
+		});
 	}
 })();
