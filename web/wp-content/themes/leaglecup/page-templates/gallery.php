fab64c8390cb595196f1e88266ba29a994c60ee6
Levron Jérémy <jeremylevron@19h47.fr>
2019-11-06 13:11:00 +0100

    :sparkles: Gallery

--- web/wp-content/themes/leaglecup/page-templates/gallery.php
+++ web/wp-content/themes/leaglecup/page-templates/gallery.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Index
+ * Template Name: Gallery
  *
  * @package LeagleCup
- * @file    index.php
+ * @file    page-templates/gallery.php
  * @author  Jérémy Levron <jeremylevron@19h47.fr> (https://19h47.fr)
  */
 
@@ -13,7 +13,7 @@ $context = Timber::context();
 
 $context['post']       = new Post();
 $context['node_type']  = 'default-page';
-$context['body_class'] = 'index';
+$context['body_class'] = 'Gallery-page';
 
 $templates = array( 'index.html.twig' );
 
