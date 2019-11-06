fee95e0b84a35446f9881060af81cee0655d1338
Levron Jérémy <jeremylevron@19h47.fr>
2019-11-06 21:15:43 +0100

    :sparkles: Carousel block

--- web/wp-content/themes/leaglecup/src/js/app.js
+++ web/wp-content/themes/leaglecup/src/js/app.js
@@ -1,6 +1,7 @@
 import GuidGuidBangBang from 'Common/GuidGuidBangBang';
 import Command from 'Blocks/Command';
 import Form from 'Blocks/Form';
+import Carousel from 'Blocks/Carousel';
 
 (() => {
 	if ('production' !== process.env.NODE_ENV) {
@@ -16,4 +17,8 @@ import Form from 'Blocks/Form';
 	//
 	const form = new Form('.js-form');
 	form.init();
+
+	//
+	const carousels = [...document.querySelectorAll('.js-carousel')];
+	carousels.map(item => new Carousel(item).init());
 })();
---
+++ web/wp-content/themes/leaglecup/src/js/utils/flickity.utilities.js
@@ -0,0 +1,54 @@
+/**
+ * Navigation next
+ *
+ * @param	obj 	slider Flickity instance
+ * @access	static
+ * @author	Jérémy Levron <jeremylevron@19h47.fr>
+ * @return	method 	Flickity next method
+ */
+export function next(slider) {
+	return slider.next();
+}
+
+
+/**
+ * Navigation previous
+ *
+ * @param	obj 	slider Flickity instance
+ * @access	static
+ * @author	Jérémy Levron <jeremylevron@19h47.fr>
+ * @return	method 	Flickity previous method
+ */
+export function previous(slider) {
+	return slider.previous();
+}
+
+
+/**
+ * Select
+ *
+ * @param obj slider     Flickity instance
+ * @param str value      Zero-based index OR selector string of the cell to
+ *                       select.
+ * @param bool isWrapped Optional. If true, the last slide will be
+ *                       selected if at the first slide.
+ * @param bool isInstant If true, immediately view the selected slide without
+ *                       animation.
+ */
+export function select(slider, value, isWrapped = false, isInstant = false) {
+	return slider.selectCell(value, isWrapped, isInstant);
+}
+
+
+/**
+ * Options object
+ *
+ * Default options, use a Object.asign to merge with other option
+ */
+export const options = {
+	contain: true,
+	groupCells: 1,
+	pageDots: false,
+	prevNextButtons: false,
+	cellSelector: '.js-item',
+};
